c2043d49670d1bead1b28273d7d12e834ac7a3ba
mdlawrenceusa <mdlawrence@me.com>
2023-04-04 10:33:53 -0400

    2 files modified, 8 added

---
+++ src/app/n-queens/n-queens.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-n-queens',
+  templateUrl: './n-queens.component.html',
+  styleUrls: ['./n-queens.component.css']
+})
+export class NQueensComponent {
+
+}
