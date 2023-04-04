286ae2fe3a7c516b047ca04903362c88f55faed3
mdlawrenceusa <mdlawrence@me.com>
2023-04-04 11:10:14 -0400

    8 Queens problem solved with recursion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
-
 import { FormsModule } from '@angular/forms';
 import { NQueensComponent } from './n-queens/n-queens.component';
 
@@ -12,14 +12,14 @@ import { NQueensComponent } from './n-queens/n-queens.component';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
     RouterModule.forRoot([
       { path: 'nqueens', component: NQueensComponent },
     ]),
   ],
   declarations: [
     AppComponent,
-
-    FormsModule,
     NQueensComponent,
   ],
   bootstrap: [AppComponent],
